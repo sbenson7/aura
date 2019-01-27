be113a5eb71dfae668248f6738dd03c45da6ac01
Scott Benson <scott.benson96@gmail.com>
2019-01-27 16:41:13 -0500

    Update subscription form to send email

--- server.js
+++ server.js
@@ -23,55 +23,77 @@ app.get('/', function (req, res) {
 
 // POST route from contact form
 app.post('/contactForm', function (req, res) {
-  try {
-    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-    var request = sg.emptyRequest({
-      method: 'POST',
-      path: '/v3/mail/send',
-      body: {
-        personalizations: [
-          {
-            to: [
-              {
-                email: process.env.EMAIL,
-              },
-            ],
-            subject: req.body.subject,
-          },
-        ],
-        from: {
-          email: req.body.email,
+  var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+  var request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: {
+      personalizations: [
+        {
+          to: [
+            {
+              email: process.env.EMAIL,
+            },
+          ],
+          subject: req.body.subject,
         },
-        content: [
-          {
-            type: 'text/plain',
-            value: req.body.fname + ' ' + req.body.lname + '(' + req.body.email + ') says: ' + req.body.message,
-          },
-        ],
+      ],
+      from: {
+        email: req.body.email,
       },
-    });
+      content: [
+        {
+          type: 'text/plain',
+          value: req.body.fname + ' ' + req.body.lname + '(' + req.body.email + ') says: ' + req.body.message,
+        },
+      ],
+    },
+  });
 
-    //With callback
-    sg.API(request, function(error, response) {
-      if (error) {
-        console.log('Error response received');
-      }
-      console.log(response.statusCode);
-      console.log(response.body);
-      console.log(response.headers);
-    });
-  } catch(error) {
-    const sgMail = require('@sendgrid/mail');
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-      to: process.env.EMAIL,
-      from: req.body.email,
-      cc: '',
-      subject: req.body.subject,
-      text: req.body.fname + ' ' + req.body.lname + '(' + req.body.email + ') says: ' + req.body.message
+  //With callback
+  sg.API(request, function(error, response) {
+    if (error) {
+      console.log('Error response received');
     }
-    sgMail.send(msg);
-  }
+  });
+res.redirect("/" );
+});
+
+// POST route from contact form
+app.post('/subscribe', function (req, res) {
+  var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+  var request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: {
+      personalizations: [
+        {
+          to: [
+            {
+              email: process.env.EMAIL,
+            },
+          ],
+          subject: 'Subscription Request',
+        },
+      ],
+      from: {
+        email: req.body.email,
+      },
+      content: [
+        {
+          type: 'text/plain',
+          value: req.body.email + ' wants to be subscribed to the newsletter.',
+        },
+      ],
+    },
+  });
+
+  //With callback
+  sg.API(request, function(error, response) {
+    if (error) {
+      console.log('Error response received');
+    }
+  });
   res.redirect("/" );
 });
 
